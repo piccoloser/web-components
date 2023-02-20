2d112689e269c116ed9dc6a78904888b244149fb
Hunter Logan <hunter.gray.logan@gmail.com>
2023-02-20 15:36:04 -0500

    Refactor

--- tabmenu.js
+++ tabmenu.js
@@ -5,33 +5,19 @@ function addElement(tagName, properties = {}) {
 class TabMenu extends HTMLElement {
     constructor() {
         super();
+
+        this.attachShadow({ mode: "open" });
+        this.wrapper = addElement("div", { id: "wrapper" });
+        this.header = addElement("header", { id: "tabs" });
+        this.open = !this.hasAttribute("closed");
+        this.divs = [];
+
+        [this.backgroundColor, this.inactiveBackgroundColor] = this.#parseBackgroundColor();
+
+        this.wrapper.appendChild(this.header);
     }
 
     connectedCallback() {
-        // Add shadow DOM.
-        this.attachShadow({ mode: "open" });
-
-        // Default background = rgba(200, 200, 200, 1)
-        let bgColorParts = [];
-        if (!this.style.backgroundColor)
-            bgColorParts.push(200, 200, 200);
-
-        // Parse user-defined background color.
-        else
-            this.style.backgroundColor.split(", ").forEach(value => {
-                bgColorParts.push(parseFloat(value.replace(/.+\(/g, "")));
-            })
-
-        // Add default alpha value if not already set.
-        if (bgColorParts.length == 3)
-            bgColorParts.push(1);
-
-        this.backgroundColor = `rgba(${bgColorParts.join(", ")})`;
-
-        // Decrease alpha value by .2 (minimum of 0) for inactive tabs.
-        let alpha = bgColorParts.pop();
-        this.inactiveBackgroundColor = `rgba(${bgColorParts.join(", ")}, ${Math.max(0, alpha - .2)})`;
-
         // Add styling.
         const style = addElement("style", {
             textContent: `
@@ -88,18 +74,12 @@ class TabMenu extends HTMLElement {
             `,
         });
 
-        this.open = !this.hasAttribute("closed");
-        this.divs = [];
-
-        const wrapper = addElement("div", { id: "wrapper" });
-        this.header = addElement("header", { id: "tabs" });
-        wrapper.appendChild(this.header);
-
+        // Add tabs to header.
         Object.entries(this.children).forEach(([n, div]) => {
             let tab = addElement("a", {
                 id: `tab-${n}`,
 
-                innerHTML: `${this.hasAttribute("numbered") ?
+                textContent: `${this.hasAttribute("numbered") ?
                     `${n}: ` : ""}${div.getAttribute("name")}`,
 
                 onclick: () => div.hasAttribute("disabled") ?
@@ -107,31 +87,29 @@ class TabMenu extends HTMLElement {
                         this.minimize() : this.openTab(n),
             });
 
+            // Handle disabled tabs.
             if (div.hasAttribute("disabled"))
                 tab.classList.add("disabled");
 
             this.header.appendChild(tab);
-            wrapper.appendChild(div);
-
+            this.wrapper.appendChild(div);
             this.divs.push([tab, div]);
         });
 
+        // Set default tab.
         if (this.open)
             this.openTab(this.getTab(this.getAttribute("open")));
 
-        this.shadowRoot.append(style, wrapper);
+        this.shadowRoot.append(style, this.wrapper);
     }
 
+    /** Returns the index of a tab given its name attribute. */
     getTab(name) {
-        let index = 0;
-        this.divs.forEach(([_, div], n) => {
-            if (div.getAttribute("name") == name)
-                index = n;
-        });
-
-        return index;
+        if (!name) return 0;
+        return this.divs.findIndex(([_, div]) => div.getAttribute("name") == name);
     }
 
+    /** Minimize the tab menu to show only the header. */
     minimize() {
         this.header.classList.add("minimized");
         this.divs.forEach(([tab, div]) => {
@@ -140,6 +118,7 @@ class TabMenu extends HTMLElement {
         });
     }
 
+    /** Open a tab given its index. */
     openTab(index) {
         this.header.classList.remove("minimized");
         this.divs.forEach(([tab, div]) => {
@@ -155,6 +134,24 @@ class TabMenu extends HTMLElement {
         });
     }
 
+    /** Handle the user-defined or default background color. */
+    #parseBackgroundColor() {
+        const DEFAULT_BG_COLOR = "rgba(200, 200, 200, 1)";
+
+        // Match numbers within the instance's background color or the default.
+        const regex = /[\d\.]+/g
+        const match = this.style.backgroundColor.match(regex) || DEFAULT_BG_COLOR.match(regex);
+
+        // Separate RGB and alpha values.
+        const bgColorParts = match ? match.map(Number) : DEFAULT_BG_COLOR;
+        const alpha = bgColorParts.length > 3 ? bgColorParts.pop() : 1;
+
+        // Create background color and inactive background color.
+        const bgColor = `rgba(${bgColorParts.join(", ")}, ${alpha})`;
+        const inactiveBgColor = `rgba(${bgColorParts.join(", ")}, ${Math.max(0, alpha - 0.2)})`;
+
+        return [bgColor, inactiveBgColor];
+    }
 }
 
 customElements.define("tab-menu", TabMenu);
